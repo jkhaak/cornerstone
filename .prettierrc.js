5878fedc68982676bf28ccc564a315c0f94a30a3
Jani Haakana <jani.haakana@icloud.com>
2023-03-16 18:07:13 +0200

    Modify default config

--- .prettierrc.js
+++ .prettierrc.js
@@ -9,12 +9,4 @@ module.exports = {
 
   // For ES5, trailing commas cannot be used in function parameters
   trailingComma: "none",
-  overrides: [
-    {
-      files: ["*.json", "*.yml", "*.yaml"],
-      options: {
-        tabWidth: 2,
-      },
-    },
-  ],
 };
