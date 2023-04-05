cd772f46e119f814628b4ffa603f37c6c01de1d3
Jani Haakana <jani.haakana@icloud.com>
2023-04-05 21:15:50 +0300

    Use specification

--- apps/ruuvi-collector/src/__test__/data-format-5.test.ts
+++ apps/ruuvi-collector/src/__test__/data-format-5.test.ts
@@ -1,9 +1,83 @@
 import * as DataFormat5 from "../model/formats/data-format-5";
+import type { Format as TDataFormat5 } from "../model/formats/data-format-5";
 import { createTestValues, TestValuesHex, TestValuesNumber, testWith, toBuffer } from "./test-util";
+
+type TestVector = [string, TDataFormat5];
 
 describe("Data format 5 specs", () => {
   const adData1 = Buffer.from("99040504aa7bb6c8f4fd0cfd4800007c76b92fa5f897846a37e6", "hex");
   const adData2 = Buffer.from("99040511623a3fc8d601c80394ffe4b356988f7adb7a25194f70", "hex");
+
+  const validData: TestVector = [
+    "99040512FC5394C37C0004FFFC040CAC364200CDCBB8334C884F",
+    {
+      manufacturerId: "499",
+      version: 5,
+      temperature: 24.3,
+      pressure: 100_044,
+      humidity: 53.49,
+      acceleration: { x: 0.004, y: -0.004, z: 1.036 },
+      power: { voltage: 2.977, tx: 4 },
+      movementCounter: 66,
+      measurementSequence: 205,
+      mac: "CBB8334C884F",
+    },
+  ];
+  const maximumData: TestVector = [
+    "9904057FFFFFFEFFFE7FFF7FFF7FFFFFDEFEFFFECBB8334C884F",
+    {
+      manufacturerId: "499",
+      version: 5,
+      temperature: 163.835,
+      pressure: 115_534,
+      humidity: 163.835,
+      acceleration: { x: 32.767, y: 32.767, z: 32.767 },
+      power: { voltage: 3.646, tx: 20 },
+      movementCounter: 254,
+      measurementSequence: 65_534,
+      mac: "CBB8334C884F",
+    },
+  ];
+  const minimumData: TestVector = [
+    "9904058001000000008001800180010000000000CBB8334C884F",
+    {
+      manufacturerId: "499",
+      version: 5,
+      temperature: -163.835,
+      pressure: 50_000,
+      humidity: 0.0,
+      acceleration: { x: -32.767, y: -32.767, z: -32.767 },
+      power: { voltage: 1.6, tx: -40 },
+      movementCounter: 0,
+      measurementSequence: 0,
+      mac: "CBB8334C884F",
+    },
+  ];
+  const invalidData: TestVector = [
+    "9904058000FFFFFFFF800080008000FFFFFFFFFFFFFFFFFFFFFF",
+    {
+      manufacturerId: "499",
+      version: 5,
+      temperature: NaN,
+      pressure: NaN,
+      humidity: NaN,
+      acceleration: { x: NaN, y: NaN, z: NaN },
+      power: { voltage: NaN, tx: NaN },
+      movementCounter: NaN,
+      measurementSequence: NaN,
+      mac: undefined,
+    },
+  ];
+
+  const testVectors: TestVector[] = [validData, maximumData, minimumData, invalidData];
+
+  it("should parse raw binary data", () => {
+    testVectors.forEach(([hexData, expected]) => {
+      const data = Buffer.from(hexData);
+
+      expect(DataFormat5.parse(data)).toStrictEqual(expected);
+    });
+  });
 
   it("should support temperature", () => {
     expect(DataFormat5.parse(adData1)).toEqual({
@@ -65,8 +139,8 @@ describe("Data format 5 specs", () => {
 
   it("should parse acceleration", () => {
     const testValues = [
-      ["fc18", -1_000],
-      ["03e8", 1_000],
+      ["fc18", -1000],
+      ["03e8", 1000],
       ["8000", NaN],
     ] satisfies TestValuesHex[];
 
@@ -111,7 +185,7 @@ describe("Data format 5 specs", () => {
     const testValues = [
       [0, 0],
       [1000, 1000],
-      [65535, NaN],
+      [65_535, NaN],
     ] satisfies TestValuesNumber[];
 
     const buffered = testValues.map(toBuffer(createTestValues("UInt16BE")));
--- apps/ruuvi-collector/src/model/formats/data-format-5.ts
+++ apps/ruuvi-collector/src/model/formats/data-format-5.ts
@@ -1,3 +1,4 @@
+import type { ValueOf } from "../utils";
 import type {
   Acceleration,
   AccelerationValue,
@@ -25,8 +26,34 @@ export type Format = {
   power: Power;
   movementCounter: MovementCounter;
   measurementSequence: MeasurementSequence;
-  mac: MACAddress;
+  mac: MACAddress | undefined;
 };
+
+type Specification<Obj> = {
+  [Key in keyof Obj]: (b: Buffer) => Obj[Key];
+};
+
+const specification: Specification<Format> = {
+  manufacturerId: () => "499",
+  version: () => 5,
+  temperature: (b) => parseTemperature(b, 3),
+  humidity: (b) => parseHumidity(b, 4),
+  pressure: (b) => parsePressure(b, 6),
+  acceleration: (b) => ({
+    x: parseAcceleration(b, 8),
+    y: parseAcceleration(b, 10),
+    z: parseAcceleration(b, 12),
+  }),
+  power: (b) => ({
+    voltage: parseBatteryVoltage(b, 14),
+    tx: parseTxPower(b, 14),
+  }),
+  movementCounter: (b) => parseMovementCounter(b, 15),
+  measurementSequence: (b) => parseMeasurementSequenceNumber(b, 17),
+  mac: (b) => b.toString("hex"),
+} satisfies Specification<Format>;
+
+const spec = Object.entries(specification);
 
 export function parseTemperature(input: Buffer, offset: number = 0): Temperature {
   const num = input.readInt16BE(offset) / 200.0;
@@ -69,19 +96,6 @@ export function parseMeasurementSequenceNumber(input: Buffer, offset: number = 0
 }
 
 export function parse(input: Buffer): RuuviData {
-  return {
-    manufacturerId: "499",
-    version: 5,
-    temperature: 0,
-    humidity: 0,
-    pressure: 0,
-    acceleration: { x: 0, y: 0, z: 0 },
-    power: {
-      voltage: 0,
-      tx: 0,
-    },
-    movementCounter: 0,
-    measurementSequence: 0,
-    mac: "",
-  };
+  const dataEntries = spec.map(([key, fn]) => [key, fn(input)]);
+  return Object.fromEntries(dataEntries);
 }
