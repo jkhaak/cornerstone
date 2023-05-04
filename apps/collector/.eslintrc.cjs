b4568e93db7dd9c5afe162d83701bec9315faab1
Jani Haakana <jani.haakana@icloud.com>
2023-05-04 20:47:44 +0300

    Init collector

---
+++ apps/collector/.eslintrc.cjs
@@ -0,0 +1,14 @@
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require("@rushstack/eslint-config/patch/modern-module-resolution");
+
+module.exports = {
+  extends: ["@rushstack/eslint-config/profile/node"],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@rushstack/typedef-var": "off",
+
+    // prefer types
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+  },
+  parserOptions: { tsconfigRootDir: __dirname },
+};
