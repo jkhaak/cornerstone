147df3f0ad6ec0be9a8498576e3939a7e1a71491
Jani Haakana <jani.haakana@icloud.com>
2023-05-06 10:52:51 +0300

    Setup database connection with dotenv

--- apps/collector/src/service.ts
+++ apps/collector/src/service.ts
@@ -1,7 +1,23 @@
-export function saveEvent(event: unknown) {
-  return 1;
+import { db } from "./database";
+import type { RuuviId } from "./model";
+
+const SQL_INSERT_RUUVITAG = `
+INSERT INTO ruuvitag
+(id, mac)
+VALUES($1, $2)
+RETURNING id;
+`;
+
+const SQL_INSERT_RUUVIEVENT = `
+INSERT INTO public.ruuvidata
+(ruuvitag, "version", datetime, temperature, humidity, pressure, acceleration_x, acceleration_y, acceleration_z, power_voltage, power_tx, movement_counter, measurement_counter)
+VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13);
+`;
+
+export async function saveEvent(event: unknown): Promise<RuuviId> {
+  return `1234`;
 }
 
-export function getEvent(id: number) {
+export async function getEvent(id: RuuviId) {
   return {};
 }
