c3ca3328942598d1050fd2c7a8e8b4fb7afdcbcd
Jani Haakana <jani.haakana@icloud.com>
2023-05-07 19:09:05 +0300

    Refactor eslint rules to common package

---
+++ tools/eslint-config/profile/default.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ["@rushstack/eslint-config/profile/node"],
+  rules: {
+    // typescript-eslint/recommended-requiring-type-checking
+    "@typescript-eslint/await-thenable": "error",
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/no-for-in-array": "error",
+    "no-implied-eval": "off",
+    "@typescript-eslint/no-implied-eval": "error",
+    "@typescript-eslint/no-misused-promises": "error",
+    "@typescript-eslint/no-unnecessary-type-assertion": "error",
+    "@typescript-eslint/no-unsafe-argument": "error",
+    "@typescript-eslint/no-unsafe-assignment": "error",
+    "@typescript-eslint/no-unsafe-call": "error",
+    "@typescript-eslint/no-unsafe-member-access": "error",
+    "@typescript-eslint/no-unsafe-return": "error",
+    "@typescript-eslint/restrict-plus-operands": "error",
+    "@typescript-eslint/restrict-template-expressions": "error",
+    "@typescript-eslint/unbound-method": "error",
+    "@typescript-eslint/no-non-null-assertion": "error",
+    "@typescript-eslint/non-nullable-type-assertion-style": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@rushstack/typedef-var": "off",
+
+    // prefer types
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+  },
+};
