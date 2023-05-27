f41b9e749a53d077474a62a77489edc1bb8d6bce
Jani Haakana <jani.haakana@icloud.com>
2023-05-27 20:24:02 +0300

    Refactor http logger and export all

--- lib/core/src/index.ts
+++ lib/core/src/index.ts
@@ -1,6 +1,8 @@
 import * as environment from "./environment";
 import logger from "./logger/logger";
-import { httpLogger } from "./logger/http-logger";
+import httpLogger from "./logger/http-logger";
 import * as datetimeTools from "./datetime";
+import * as promises from "./promises";
+import * as filterTools from "./filter-tools";
 
-export { environment, logger, httpLogger, datetimeTools };
+export { environment, logger, httpLogger, datetimeTools, promises, filterTools };
--- lib/core/src/logger/http-logger.ts
+++ lib/core/src/logger/http-logger.ts
@@ -1,6 +1,4 @@
 import { pinoHttp as pinoHttpInit } from "pino-http";
 import { opts } from "./logger";
 
-const httpLogger = pinoHttpInit(opts);
-
-export { httpLogger };
+export default pinoHttpInit(opts);
