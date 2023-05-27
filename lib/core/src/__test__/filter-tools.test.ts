7cf5a14cecf7b4c42c92e68a8dd3e8dd43187dd5
Jani Haakana <jani.haakana@icloud.com>
2023-05-27 20:23:09 +0300

    Add notUndefined to filter tools

---
+++ lib/core/src/__test__/filter-tools.test.ts
@@ -0,0 +1,13 @@
+import { notUndefined } from "../filter-tools";
+import { expectTypeOf } from "expect-type";
+
+describe("filter tools", () => {
+  it("should have a function to filter undefined values", () => {
+    const values = [1, undefined, 2, 3, undefined];
+    const result = values.filter(notUndefined);
+    expect(result).toStrictEqual([1, 2, 3]);
+    expectTypeOf(result).toEqualTypeOf<number[]>();
+    expectTypeOf(result).not.toEqualTypeOf<undefined[]>();
+    expectTypeOf(result).not.toEqualTypeOf<Array<number | undefined>>();
+  });
+});
