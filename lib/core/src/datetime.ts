e3afe123825d63e75ad4cf13619f69d2fbfd7dff
Jani Haakana <jani.haakana@icloud.com>
2023-06-03 21:53:52 +0300

    Remove unneeded code

--- lib/core/src/datetime.ts
+++
@@ -1,6 +0,0 @@
-import { formatInTimeZone } from "date-fns-tz";
-
-export function format(date: Date | number) {
-  const d = date instanceof Date ? date : new Date(date);
-  return formatInTimeZone(d, "UTC", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-}
--- lib/core/src/index.ts
+++ lib/core/src/index.ts
@@ -1,8 +1,7 @@
 import * as environment from "./environment";
 import logger from "./logger/logger";
 import httpLogger from "./logger/http-logger";
-import * as datetimeTools from "./datetime";
 import * as promises from "./promises";
 import * as filterTools from "./filter-tools";
 
-export { environment, logger, httpLogger, datetimeTools, promises, filterTools };
+export { environment, logger, httpLogger, promises, filterTools };
