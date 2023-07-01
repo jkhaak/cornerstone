7003e48e22f17bc88eb2ac261070873341b34964
Jani Haakana <jani.haakana@icloud.com>
2023-07-01 17:56:01 +0300

    Add rollup config

---
+++ apps/ble-reader/rollup.config.mjs
@@ -0,0 +1,14 @@
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "dist/bundle.js",
+    format: "cjs",
+    sourcemap: true,
+  },
+  plugins: [typescript(), json(), resolve(), commonjs()],
+};
