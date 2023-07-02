6264e8063a9325de4c5fde6c6d7eaaa3098f5598
Jani Haakana <jani.haakana@icloud.com>
2023-07-02 18:19:58 +0300

    Add function to read from file

--- apps/ble-reader/src/__test__/config.test.ts
+++ apps/ble-reader/src/__test__/config.test.ts
@@ -1,8 +1,37 @@
-import { configToEnvironment } from "../config";
+import { parseConfig, configToEnvironment } from "../config";
+import fs from "node:fs";
+import os from "node:os";
 
 describe("config", () => {
+  const config = { foo: "bar", bar: "baz", baz: true, quux: 123 };
+
+  it("should parse config file and produce config object", () => {
+    const configPath = `${os.tmpdir()}/config.json`;
+    fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
+
+    parseConfig(configPath);
+
+    for (const [key, val] of Object.entries(config)) {
+      expect(process.env[key.toUpperCase()]).toBe(val.toString());
+    }
+
+    fs.unlinkSync(configPath);
+  });
+
+  it("should throw error if config file does not exist", () => {
+    expect(() => parseConfig("does-not-exist.json")).toThrowError(/Could not read config file/);
+  });
+
+  it("should throw error if config file is not valid JSON", () => {
+    const configPath = `${os.tmpdir()}/config.json`;
+    fs.writeFileSync(configPath, "this is not valid JSON");
+
+    expect(() => parseConfig(configPath)).toThrowError(/Could not parse config file/);
+
+    fs.unlinkSync(configPath);
+  });
+
   it("should read config object and produce environment variables", () => {
-    const config = { foo: "bar", bar: "baz", baz: true, quux: 123 };
     configToEnvironment(config);
 
     // eslint-disable-next-line dot-notation
--- apps/ble-reader/src/config.ts
+++ apps/ble-reader/src/config.ts
@@ -1,6 +1,9 @@
 import { environment } from "@cornerstone/core";
+import fs from "node:fs";
 
-export function configToEnvironment(config: Record<string, string | number | boolean>) {
+type Config = Record<string, string | number | boolean>;
+
+export function configToEnvironment(config: Config) {
   for (const [key, val] of Object.entries(config)) {
     if (typeof val === "string") {
       environment.setEnv(key, val);
@@ -8,4 +11,23 @@ export function configToEnvironment(config: Record<string, string | number | boo
       environment.setEnv(key, val.toString());
     }
   }
+}
+
+export function parseConfig(path: string) {
+  let rawString;
+  let obj;
+
+  try {
+    rawString = fs.readFileSync(path, "utf-8");
+  } catch (e: unknown) {
+    throw new Error(`Could not read config file at ${path}`);
+  }
+
+  try {
+    obj = JSON.parse(rawString) as Config;
+  } catch (e: unknown) {
+    throw new Error(`Could not parse config file at ${path}`);
+  }
+
+  configToEnvironment(obj);
 }
