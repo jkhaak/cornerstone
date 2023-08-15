4176655b36c9a75164bface6059ab51ad4b31b12
Jani Haakana <jani.haakana@icloud.com>
2023-08-15 15:00:01 +0300

    Add crude test for endpoint

---
+++ apps/ble-reader/src/__test__/endpoint.test.ts
@@ -0,0 +1,21 @@
+import type { Mqtt } from "@cornerstone/mqtt";
+import { sendEvent } from "../services/endpoint.js";
+
+const mqttMock = {
+  publish: jest.fn(),
+};
+const mqtt = mqttMock as unknown as Mqtt;
+
+describe("endpoint", () => {
+  it("should pass topic and message to mqtt", () => {
+    const topic = "topic";
+    const message = { message: "message" };
+
+    const eventHandler = sendEvent(mqtt);
+    expect(mqttMock.publish.mock.calls.length).toBe(0);
+
+    eventHandler(topic, message);
+    expect(mqttMock.publish).toHaveBeenCalledWith(topic, JSON.stringify(message));
+    expect(mqttMock.publish.mock.calls.length).toBe(1);
+  });
+});
